59c52bdd6b6d53f1be7ecab6eb74c596e6e17858
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-12-08 02:23:49 +0400

    license title

--- jake/model.js
+++ jake/model.js
@@ -1,6 +1,5 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-// import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 
 document.getElementById('loadingMessage').style.display = 'block';
 
