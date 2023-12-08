9c3310508a804385cda36eee20650714d72a626c
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-12-08 04:00:05 +0400

    Jake model  code refactoring

--- jake/model.js
+++ jake/model.js
@@ -1,29 +1,19 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.getElementById("container3D").appendChild(renderer.domElement);
 document.getElementById('loadingMessage').style.display = 'block';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.9, 100);
-let object;
-let objToRender = 'jake';
-const loader = new GLTFLoader();
-
-loader.load(
-  `models/${objToRender}/scene.gltf`,
-  function (gltf) {
-    document.getElementById('loadingMessage').style.display = 'none';
-    object = gltf.scene;
-    scene.add(object);
-  }
-);
-
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("container3D").appendChild(renderer.domElement);
-
 camera.position.z = 50;
 camera.position.y = -20;
+let object;
+let objToRender = 'jake';
 
 const pointLight = new THREE.PointLight(0xffffff, 10);
 pointLight.position.set(0, 0, 150);
@@ -35,6 +25,22 @@ scene.add(ambientLight);
 const hemiLight = new THREE.HemisphereLight(0xffe5bb, 0x080820, 1);
 scene.add(hemiLight);
 
+const loader = new GLTFLoader();
+
+loader.load(
+  `models/jake/scene.gltf`,
+  function (gltf) {
+    document.getElementById('loadingMessage').style.display = 'none';
+    object = gltf.scene;
+    scene.add(object);
+  }
+);
+
+
+//анимация модели
+
+
+//вращение по оси
 function animate() {
   requestAnimationFrame(animate);
 
@@ -43,9 +49,11 @@ function animate() {
   }
   renderer.render(scene, camera);
 }
+animate();
 
+
+//вращение по другому правилу при щелчке мыши
 let isRotating = false;
-
 function rotateObject() {
   if (object && objToRender === "jake" && isRotating) {
     object.rotation.x += 0.01;
@@ -54,23 +62,21 @@ function rotateObject() {
   renderer.render(scene, camera);
   requestAnimationFrame(rotateObject);
 }
-
 function toggleRotation() {
   isRotating = !isRotating;
 }
-
 const container = document.getElementById("container3D");
 container.addEventListener("click", toggleRotation);
-
 rotateObject();
 
+
+//изменение размера при изменении размера окна
 window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-animate();
 
 
 
