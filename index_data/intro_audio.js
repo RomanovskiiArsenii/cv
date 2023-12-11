6ccbef0e35a27283e992bd004c468452469c0a10
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-12-11 23:52:08 +0400

    loader rebuild

--- index_data/intro_audio.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function () {
-    const audio = new Audio('index_data/intro_audio.mp3');
-    audio.play();
-});
-
--- index_data/loader.js
+++ index_data/loader.js
@@ -1,13 +1,20 @@
-window.addEventListener('load', function () {
+const backgroundImageUrl = 'index_data/images/header_ink.gif';
+
+function loadImage(url, callback) {
+    const image = new Image();
+    image.onload = callback;
+    image.src = url;
+}
+
+loadImage(backgroundImageUrl, function () {
     const hiddenElement = document.querySelector('.hidden');
     if (hiddenElement) {
         hiddenElement.classList.remove('hidden');
     }
-});
-
-window.addEventListener('load', function () {
     const animatedSvgElement = document.querySelector('.animated-svg');
     if (animatedSvgElement) {
         animatedSvgElement.classList.add('hidden');
     }
+    const audio = new Audio('index_data/intro_audio.mp3');
+    audio.play();
 });
