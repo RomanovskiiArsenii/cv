6d9acbf67ffb339e5e46b49673d28127eadd1296
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-12-24 03:25:40 +0400

    Version 4
    New intro photo, new loader and loading logic, circles design, bottom circle gets bigger + get v line.

--- index_data/animation_loading.js
+++ index_data/animation_loading.js
@@ -1,14 +1,50 @@
-//анимация загрузчика и сокрытие страницы до завершения загрузки header_ink.gif
+//анимация загрузчика и сокрытие страницы до завершения загрузки img_1_cut.jpg
+// const backgroundImageUrl = 'index_data/images/img_1_cut.jpg';
 
-const backgroundImageUrl = 'index_data/images/header_ink.gif';
+// function loadImage(url, callback) {
+//     const image = new Image();
+//     image.onload = callback;
+//     image.src = url;
+// }
 
-function loadImage(url, callback) {
-    const image = new Image();
-    image.onload = callback;
-    image.src = url;
+// loadImage(backgroundImageUrl, function () {
+//     const hiddenElement = document.querySelector('.hidden');
+//     if (hiddenElement) {
+//         hiddenElement.classList.remove('hidden');
+//     }
+//     const animatedSvgElement = document.querySelector('.loading_animation');
+//     if (animatedSvgElement) {
+//         animatedSvgElement.classList.add('hidden');
+//     }
+//     // const audio = new Audio('index_data/intro_audio.mp3');
+//     // audio.play();
+// });
+
+function executeAfterBackgroundImagesLoaded(callback) {
+    const elements = document.querySelectorAll('*');
+    const imagesToLoad = [];
+    let imagesLoaded = 0;
+
+    function checkAllImagesLoaded() {
+        imagesLoaded++;
+        if (imagesLoaded === imagesToLoad.length) {
+            callback();
+        }
+    }
+
+    elements.forEach(element => {
+        const backgroundImage = window.getComputedStyle(element).backgroundImage;
+        const imageURL = backgroundImage.match(/\burl\((.*?)\)/);
+        if (imageURL) {
+            const img = new Image();
+            img.src = imageURL[1].replace(/["']/g, '');
+            img.onload = checkAllImagesLoaded;
+            imagesToLoad.push(img);
+        }
+    });
 }
 
-loadImage(backgroundImageUrl, function () {
+executeAfterBackgroundImagesLoaded(function () {
     const hiddenElement = document.querySelector('.hidden');
     if (hiddenElement) {
         hiddenElement.classList.remove('hidden');
@@ -17,6 +53,4 @@ loadImage(backgroundImageUrl, function () {
     if (animatedSvgElement) {
         animatedSvgElement.classList.add('hidden');
     }
-    // const audio = new Audio('index_data/intro_audio.mp3');
-    // audio.play();
 });
