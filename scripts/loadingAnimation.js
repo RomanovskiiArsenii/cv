f62972cd007a522a2da1fd4d1c5812dd09829c75
RomanovskiiArsenii <romanovskyway@gmail.com>
2024-03-21 05:35:49 +0400

    cassette loader

--- scripts/loadingAnimation.js
+++ scripts/loadingAnimation.js
@@ -1,19 +1,25 @@
 const loadingAnimation = (() => {
     const hidden = document.getElementById('hidden_untill_loaded');
     const showed = document.getElementById('showed_untill_load');
+    const cassette = document.getElementById('cassette');
     const introStripArray = document.getElementsByClassName('intro_strip');
     const outroStripArray = document.getElementsByClassName('outro_strip');
 
     const onLoadMain = () => {
-        showed.style.display = 'none';
-        hidden.style.display = 'block';
+        setTimeout(() => {
+            cassette.style.animation = 'cassetteFadeOut .3s ease forwards';
+        }, 1000);
+        setTimeout(() => {
+            showed.style.display = 'none';
+            hidden.style.display = 'block';
+        }, 1300);
         setTimeout(() => {
             for (let i = 0; i < introStripArray.length; i++) {
                 setTimeout(() => {
                     introStripArray[i].style.animation = 'introMove 0.6s ease 0.6s forwards';
                 }, i * 100);
             }
-        }, 500);
+        }, 1500);
     };
 
     const onLeaveMain = (e) => {
