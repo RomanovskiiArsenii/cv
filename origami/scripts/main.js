c29f51578c20bdd7e0ef9e34a90691ec3babb8fb
RomanovskiiArsenii <romanovskyway@gmail.com>
2024-04-20 16:00:36 +0400

    delete origami

--- origami/scripts/main.js
+++
@@ -1,7 +0,0 @@
-import loaderControl from './loader.js';
-import productsControl from './products.js';
-import galleryControl from './gallery.js';
-
-window.addEventListener('load', loaderControl);
-window.addEventListener('load', productsControl);
-window.addEventListener('load', galleryControl);
